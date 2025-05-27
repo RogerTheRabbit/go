f7517319fab197e8f59a0a2fdfd4a69086732a46
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2025-05-27 00:40:16 -0400

    Start of creating UI to manage go links

--- app.js
+++ app.js
@@ -5,20 +5,27 @@ const app = express();
 const port = 3000;
 
 const fs = require("node:fs");
+const data = fs.readFileSync(fp.resolve(__dirname, "config.csv"), "utf8");
+const links = [];
+// Support new lines in windows and linux
+data.split(/[\r\n]+/).forEach((line) => {
+  entry = line.split(",");
+  if (entry.length == 2) {
+    links.push({ key: entry[0].trim(), url: entry[1].trim() });
+  }
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use(function (req, res, next) {
+  if (req.path === "/go" || req.path.startsWith("/go/")) {
+    next();
+    return;
+  }
+
   let found = "https://kserver.uk/homelab-dashboard/assets/unavailable.svg";
   try {
-    const data = fs.readFileSync(fp.resolve(__dirname, "config.csv"), "utf8");
-    const links = [];
-    // Support new lines in windows and linux
-    data.split(/[\r\n]+/).forEach((line) => {
-      entry = line.split(",");
-      if (entry.length == 2) {
-        links.push({ key: entry[0].trim(), url: entry[1].trim() });
-      }
-    });
-
     path = req.path.substring(1);
 
     if (path === "") {
@@ -52,6 +59,32 @@ app.use(function (req, res, next) {
 
 app.get("/", (req, res) => {
   res.send("<a href='http://go'>Setup go links here!<a>");
+});
+
+app.get("/go", (req, res) => {
+  res.sendFile(fp.join(__dirname + "/go.html"));
+});
+
+app.get("/go", (req, res) => {
+  res.sendFile(fp.join(__dirname + "/go.html"));
+});
+
+app.get("/go/favicon", (req, res) => {
+  res.sendFile(fp.join(__dirname + "/favicon.png"));
+});
+
+app.get("/go/links", (req, res) => {
+  res.send(links);
+});
+
+app.post("/go/links", (req, res) => {
+  const body = req.body;
+  links.push({ key: body["path"].trim(), url: body["url"].trim() });
+});
+
+app.delete("/go/links", (req, res) => {
+  const body = req.body;
+  links.push({ key: body["path"].trim(), url: body["url"].trim() });
 });
 
 app.listen(port, () => {
